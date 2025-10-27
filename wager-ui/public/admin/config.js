f6e85e0776e27049e6a0c69c62da60e803d8ceee
Muhammad Obaid Ullah Khan <87824161+Oba1d-Khan@users.noreply.github.com>
2025-10-27 23:46:01 +0500

    fix: Hardcode static config.js API base URL to Render deployment for final test

--- wager-ui/public/admin/config.js
+++ wager-ui/public/admin/config.js
@@ -1,11 +1,20 @@
-const domainConfig = { 'dev': { API_BASE_URL: 'https://dev.wagervs.fun/api', BASE_URL: 'https://dev.wagervs.fun', }, }
+const domainConfig = {
+  dev: {
+    API_BASE_URL: "https://dev.wagervs.fun/api",
+    BASE_URL: "https://dev.wagervs.fun",
+  },
+};
 
 // Get the current domain
 const currentDomain = window.location.hostname;
 
 // Check if currentDomain exists in the domainConfig, otherwise fallback to the default
-window.API_BASE_URL = (window.location.hostname === "localhost" && window.location.port === "3000") ? "http://localhost:5000/api" : "/api";
+// window.API_BASE_URL =
+//   window.location.hostname === "localhost" && window.location.port === "3000"
+//     ? "http://localhost:5000/api"
+//     : "/api";
+window.API_BASE_URL = "https://wagervsmono-test.onrender.com/api";
+//
 const BASE_URL = "";
 
 const DISTRIBUTION_FEE_PERCENTAGE = 1;
-
